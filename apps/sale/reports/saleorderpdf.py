c859f7293cabd1003f79112463cee93ac89fccba
gvizquel <gvizquel@gmail.com>
2019-08-29 17:38:22 -0400

    Reporte: Sale Order

--- apps/sale/reports/saleorderpdf.py
+++ apps/sale/reports/saleorderpdf.py
@@ -22,14 +22,14 @@ def sale_order_pdf(request, pk):
     """ Función para imprimir la orden de ventas
     """
     response = HttpResponse(content_type='application/pdf')
-    pdf_name = "clientes.pdf"  # llamado clientes
+    # pdf_name = "clientes.pdf"  # llamado clientes
     # response['Content-Disposition'] = 'attachment; filename=%s' % pdf_name
 
     # Los productos de la orden
     _sale_order = PySaleOrder.objects.get(pk=pk)
 
     # Los productos de la orden ya en una matriz
-    _products = PySaleOrderDetail.objects.filter(sale_order=pk).values(
+    _products = PySaleOrderDetail.objects.filter(sale_order=pk).only(
         "product",
         "description",
         "quantity",
@@ -48,11 +48,10 @@ def sale_order_pdf(request, pk):
     archivo_imagen = settings.MEDIA_ROOT+'/pyerp-marketing/PyERP_logo_ 2.png'
     # Definimos el tamaño de la imagen a cargar y las coordenadas
     _pdf.drawImage(archivo_imagen, 30, 710, 120, 90, preserveAspectRatio=True)
+    _pdf.setLineWidth(.3)
     _pdf.line(30, 735, 582, 735)
 
     # Header del reporte
-    _pdf.setLineWidth(.3)
-    # presupesto_num = 'Presupuesto # ' + _sale_order.name
     _pdf.setFont('Helvetica', 22)
     _pdf.drawString(30, 650, 'Presupuesto # ' + _sale_order.name)
 
@@ -78,7 +77,7 @@ def sale_order_pdf(request, pk):
     # Header de la tabla
     _data_header = []
     _data_header.append([
-        "Producto",
+        # "Producto",
         "Descripcion",
         "Cantidad",
         "Precio",
@@ -89,7 +88,7 @@ def sale_order_pdf(request, pk):
     # Imprimir el header de la tabla
     table = Table(
         _data_header,
-        colWidths=[2*cm, 6*cm, 2*cm, 3*cm, 3*cm, 3.5*cm]
+        colWidths=[7*cm, 3*cm, 3*cm, 3*cm, 3.5*cm]
     )
     table.setStyle(
         TableStyle([
@@ -99,33 +98,33 @@ def sale_order_pdf(request, pk):
             ('LINEBELOW', (0, 0), (-1, 0), 1.5, colors.black),
             # ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
             ('VALIGN', (0, 0), (-1, 0), 'MIDDLE'),
-            ('ALIGN', (2, 0), (-1, 0), 'RIGHT'),
+            ('ALIGN', (1, 0), (-1, 0), 'RIGHT'),
             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('FONTSIZE', (0, 0), (-1, 0), 9),
+            ('FONTSIZE', (0, 0), (-1, 0), 10),
         ])
     )
     table.wrapOn(_pdf, _width, _height)
     table.drawOn(_pdf, 30, _high)
 
-    # Agregamos los productos a la tabla
+    # Cuerpo de la tabla
     _data_table = []
     for _product in _products:
         _data_table.append(
             [
-                _product["product"],
-                _product["description"],
-                str(_product["quantity"]) + ' Unidades',
-                _product["amount_untaxed"],
-                _product["discount"],
-                _product["amount_total"]
+                _product.product,
+                # _product.description,
+                str(_product.quantity) + ' Unidades',
+                _product.amount_untaxed,
+                _product.discount,
+                _product.amount_total
             ]
         )
         _high = _high - 18
 
-    # Imprimir la tabla
+    # Imprimir cuerpo la tabla
     table = Table(
         _data_table,
-        colWidths=[2*cm, 6*cm, 2*cm, 3*cm, 3*cm, 3.5*cm]
+        colWidths=[7*cm, 3*cm, 3*cm, 3*cm, 3.5*cm]
     )
     table.setStyle(
         TableStyle([
@@ -133,14 +132,54 @@ def sale_order_pdf(request, pk):
             # ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
             ('LINEBELOW', (0, 0), (-1, -1), 0.5, colors.black),
             ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-            ('ALIGN', (0, 0), (0, -1), 'CENTER'),
-            ('ALIGN', (2, 0), (-1, -1), 'RIGHT'),
+            # ('ALIGN', (0, 0), (0, -1), 'CENTER'),
+            ('ALIGN', (1, 0), (-1, -1), 'RIGHT'),
             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica'),
-            ('FONTSIZE', (0, 0), (-1, -1), 9),
+            ('FONTSIZE', (0, 0), (-1, -1), 10),
         ])
     )
     table.wrapOn(_pdf, _width, _height)
     table.drawOn(_pdf, 30, _high)
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+
+    # Footer de la tabla
+    _data_foot = []
+    _data_foot.append(
+        ["", "Subtotal:", "$" + "{:.2f}".format(_sale_order.amount_untaxec)]
+    )
+    _data_foot.append(
+        ["", "IVA:", "$" + "???"]
+    )
+    _data_foot.append(
+        ["", "Total:", "$" + "???"]
+    )
+
+    _high = _high - (18 * 3)
+    # Imprimir cuerpo la tabla
+    table = Table(
+        _data_foot,
+        colWidths=[13*cm, 3*cm, 3.5*cm]
+    )
+    table.setStyle(
+        TableStyle([
+            # ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+            # ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+            ('LINEBELOW', (1, 0), (2, 0), 1.5, colors.black),
+            ('LINEBELOW', (1, 1), (2, 1), 0.5, colors.black),
+            ('LINEBELOW', (1, 2), (2, 2), 1.5, colors.black),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            # ('ALIGN', (0, 0), (0, -1), 'CENTER'),
+            ('ALIGN', (1, 0), (-1, -1), 'RIGHT'),
+            ('FONTNAME', (1, 0), (1, 0), 'Helvetica-Bold'),
+            ('FONTSIZE', (0, 0), (-1, -1), 10),
+            ('FONTNAME', (1, 2), (1, 2), 'Helvetica-Bold'),
+        ])
+    )
+    table.wrapOn(_pdf, _width, _height)
+    table.drawOn(_pdf, 30, _high)
+
+
 
     # Close the PDF object cleanly, and we're done.
     _pdf.showPage()
